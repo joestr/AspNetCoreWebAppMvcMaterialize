6281c1c18ee7633330d40938d4d198713e2588ad
Joel Strasser <j.strasser@leeb-balkone.com>
2024-09-03 17:37:27 +0200

    feat: include dark mode and light mode switching logic

---
+++ AspNetCoreWebAppMvcMaterialize/wwwroot/js/color-scheme-preference.js
@@ -0,0 +1,16 @@
+﻿const theme = localStorage.getItem('theme');
+if (theme) {
+    // Use theme present in localStorage
+    document.documentElement.setAttribute('theme', theme);
+} else {
+    // Theme is not present in localStorage, query operating system preferences
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // Dark mode
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.setAttribute('theme', localStorage.getItem('theme'));
+    } else {
+        // Light mode
+        localStorage.setItem('theme', 'light');
+        document.documentElement.setAttribute('theme', localStorage.getItem('theme'));
+    }
+}
---
+++ AspNetCoreWebAppMvcMaterialize/wwwroot/js/color-scheme-switcher.js
@@ -0,0 +1,41 @@
+﻿// Change Theme Setting with a Switch
+const currentTheme = localStorage.getItem('theme');
+const switchElem = document.querySelector('#theme-switch');
+
+const setTheme = (isDark) => {
+    if (isDark) {
+        switchElem.classList.add('is-dark');
+        switchElem.querySelector('i').innerText = 'light_mode';
+        switchElem.title = 'Switch to light mode';
+    } else {
+        switchElem.classList.remove('is-dark');
+        switchElem.querySelector('i').innerText = 'dark_mode';
+        switchElem.title = 'Switch to dark mode';
+    }
+}
+
+if (switchElem) {
+    // Load
+    if (currentTheme === 'dark') {
+        setTheme(true);
+    } else {
+        setTheme(false);
+    }
+
+    // Change
+    switchElem.addEventListener('click', e => {
+        e.preventDefault();
+        if (!switchElem.classList.contains('is-dark')) {
+            // Dark Theme
+            document.documentElement.setAttribute('theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            setTheme(true);
+        }
+        else {
+            // Light Theme
+            document.documentElement.setAttribute('theme', 'light');
+            localStorage.setItem('theme', 'light');
+            setTheme(false);
+        }
+    });
+}
