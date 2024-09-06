0b0f68481d9339e9cc18c3df5778961a29cf8f11
Joel Strasser <j.strasser@leeb-balkone.com>
2024-09-06 07:50:21 +0200

    feat: move privacy page to own controller

---
+++ AspNetCoreWebAppMvcMaterialize/Controllers/PrivacyController.cs
@@ -0,0 +1,21 @@
+using AspNetCoreWebAppMvcMaterialize.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreWebAppMvcMaterialize.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        private readonly ILogger<PrivacyController> _logger;
+
+        public PrivacyController(ILogger<PrivacyController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
