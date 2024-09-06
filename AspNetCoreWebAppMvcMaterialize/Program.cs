821bd0d582951d950009540440af3fc4ee71325a
Joel Strasser <j.strasser@leeb-balkone.com>
2024-09-06 07:48:18 +0200

    feat: move error page to own controller

--- AspNetCoreWebAppMvcMaterialize/Program.cs
+++ AspNetCoreWebAppMvcMaterialize/Program.cs
@@ -14,7 +14,7 @@ namespace AspNetCoreWebAppMvcMaterialize
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error/Index");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
